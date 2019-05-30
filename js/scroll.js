0545a374a986b92a89a21ff6244f849b9a758b8d
mahmood fara <mahmoodsagharjooghi@gmail.com>
2019-05-30 21:52:28 +0430

    join us added, nav-bar fixed and ...

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-			ScrollReveal().reveal('.hero-content' , {delay:'2000', distance: '10%', origin: 'bottom' , opacity: '0'});
+			ScrollReveal().reveal('.hero-content' , {duration: 1000, delay:'2000', distance: '10%', origin: 'bottom' , opacity: '0'});
 			ScrollReveal().reveal('.section-title h2' , {duration: 1000,delay:200, distance: '70%', origin: 'bottom' , opacity: '0'});
 			ScrollReveal().reveal('.about-contant .container .row .col-md-6:nth-child(1)' , {duration: 1000,delay:1000, distance: '30%', origin: 'left' , opacity: '0'});
 			ScrollReveal().reveal('.about-contant .container .row .col-md-6:nth-child(2)' , {duration: 1000,delay:1000, distance: '30%', origin: 'right' , opacity: '0'});
@@ -44,4 +44,8 @@ $(document).ready(function() {
 			ScrollReveal().reveal('.services-card-section .row .col-md-4:nth-child(2)' , {duration: 1000,delay:250 , opacity: '0', distance: '30%' , origin: 'bottom'});
 			ScrollReveal().reveal('.services-card-section .row .col-md-4:nth-child(3)' , {duration: 1000,delay:450 , opacity: '0', distance: '30%' , origin: 'bottom'});
 
+
+			// Let's talk section 
+			ScrollReveal().reveal('.lets-talk-section h1' , {duration: 1000,delay:150 , opacity: '0', distance: '70%' , origin: 'bottom'});
+			lets-talk-section
 			});
